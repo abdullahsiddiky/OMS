79e66b818e8fd4290297aeb4c360e5d24d32d26a
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2024-02-16 22:07:44 +0600

    working on registration component

--- frontend/src/app/Components/Login.tsx
+++ frontend/src/app/Components/Login.tsx
@@ -2,7 +2,6 @@ export default function Login(){
    return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      
     </div>
 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
